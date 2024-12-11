3748766673b8f8ddec15d52a5f9968ab7f014b6c
Leonardo Alarcon <alarcon842@hotmail.com>
2024-12-10 19:43:04 -0600

    Menú principal de interacción de usuario

--- src/Moneda.java
+++ src/Moneda.java
@@ -5,5 +5,5 @@ import java.util.Map;
 public record Moneda(String result,
                      String time_last_update_utc,
                      String base_code, Map<String,
-        String> conversion_rates) {
+                     String> conversion_rates) {
 }
--- src/Principal.java
+++ src/Principal.java
@@ -1,9 +1,53 @@
+import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) {
 
-        ConsultaMoneda consulta = new ConsultaMoneda();
-        Moneda moneda = consulta.buscarMoneda("USD");
-        System.out.println(moneda);
+        Scanner scanner = new Scanner(System.in);
+        ConvercionDeMonedas valor = new ConvercionDeMonedas();
+        double cantidad;
+        String monedaBase;
+        String conversor;
+        var salir = "Elegiste salir del sistema.....";
+
+        var menu = """
+                
+                *****************************************************
+                
+                :::::> COVERSOR DE MONEDAS BIENVENIDO <::::
+                
+                ||| Por favor ingresa el valor de dinero que deseas
+                    covertir y despues moneda origen  y destino como
+                    (ej. USD).                                      ||
+                    
+                   >> Para salir del programa ingrese -1 en el monto <<
+                
+                *******************************************************
+                
+                """;
+
+        try {
+            while (true){
+                System.out.println(menu);
+                System.out.println("Ingrese el monto: ");
+                cantidad = Double.parseDouble(scanner.nextLine());
+                if (cantidad == -1){
+                    System.out.println(salir);
+                    return;
+                }
+                System.out.println("Ingrese la monmeda de origen: ");
+                monedaBase = scanner.nextLine().toUpperCase().strip();
+
+                System.out.print("Ingrese la moneda destino: ");
+                conversor = scanner.nextLine().toUpperCase().strip();
+
+                valor.valorAConvertir(cantidad,monedaBase, conversor);
+
+
+            }
+
+        }catch (Exception e) {
+            System.out.println("Ingrese la moneda destino: " + e.getMessage());
+        }
     }
 }
